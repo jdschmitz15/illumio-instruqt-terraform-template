cd863208431de18d329db0c8ad16b5d4cf72b592
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-06-30 16:04:32 -0400

    other

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">=0.12"
   required_providers {
-    # azapi = {
-    #   source  = "azure/azapi"
-    #   version = "~>1.5"
-    # }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
     # azurerm = {
     #   source  = "hashicorp/azurerm"
     #   version = "~>2.0"
