c270f4d1b67c45c4503971855f58f55d994e6c6a
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-06-30 14:49:53 -0400

    update Illumio_provider

--- Illumio_provider.tf
+++ Illumio_provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    illumio-cloudsecure = {
+      source  = "illumio/illumio-cloudsecure"
+      version = ">= 1.0.11"
+    }
+  }
+}
+
 provider "illumio-cloudsecure" {
   client_id     = var.illumio_cloudsecure_client_id
   client_secret = var.illumio_cloudsecure_client_secret
