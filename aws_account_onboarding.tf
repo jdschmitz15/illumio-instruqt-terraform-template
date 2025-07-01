f0680cfd2f25d5b0f247f96668d2ef225dab0e95
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-06-30 20:21:06 -0400

    aws iam prefix

--- aws_account_onboarding.tf
+++ aws_account_onboarding.tf
@@ -2,6 +2,7 @@ module "aws_account" {
   source  = "illumio/cloudsecure/illumio//modules/aws_account"
   version = "1.5.1"
   name    = "Test Account"
+  iam_name_prefix       = random_string.random.id
   tags    = {
     Name  = "CloudSecure Account Policy"
     Owner = "Engineering"
