cd50dd6763cebc43982d6c7e6afa917de770e827
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-06-30 21:33:03 -0400

    Save

--- aws_account_onboarding.tf
+++ aws_account_onboarding.tf
@@ -1,7 +1,7 @@
 module "aws_account" {
   source  = "illumio/cloudsecure/illumio//modules/aws_account"
   version = "1.5.1"
-  name    = "Test Account"
+  name    = "Instruqt AWS Account"
   iam_name_prefix       = random_string.random.id
   tags    = {
     Name  = "CloudSecure Account Policy"
