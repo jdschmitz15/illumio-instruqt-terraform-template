106a8b015ba4dc41ba359232f835beea7324fd6a
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-06-30 21:29:15 -0400

    Static

--- network_azure.tf
+++ network_azure.tf
@@ -44,7 +44,7 @@ resource "azurerm_public_ip" "JumpIP" {
   name                = "JumpIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku = "Basic"
-  allocation_method   = "Dynamic"
+  sku = "Standard"
+  allocation_method   = "Static"
 }
 
