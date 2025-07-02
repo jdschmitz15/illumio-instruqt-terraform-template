a7558442548b7f37452cd55580b244460d224ba5
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-07-02 09:13:29 -0400

    testing

---
+++ test/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+provider "illumio-cloudsecure" {
+  client_id     = var.illumio_cloudsecure_client_id
+  client_secret = var.illumio_cloudsecure_client_secret
+}
+
+module "aws_account_dev" {
+  source  = "illumio/cloudsecure/illumio//modules/aws_account"
+  version = "1.5.1"
+  name    = "Test Account"
+  tags    = {
+    Name  = "CloudSecure Account Policy"
+    Owner = "Engineering"
+  }
+}
