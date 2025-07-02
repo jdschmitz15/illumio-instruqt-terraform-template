c97eda951a0462e05bc0dcd7a08a7783625567ec
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-07-01 21:07:35 -0400

    Testing

--- aws/public_subnet_resources_aws.tf
+++ aws/public_subnet_resources_aws.tf
@@ -27,6 +27,25 @@ resource "aws_instance" "jumphost01" {
     (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.0.3.33 3306 -t 10 >> /tmp/DB.log") | crontab -
     (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.0.3.23 3306 -t 10 >> /tmp/DB.log") | crontab -
     (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.0.2.43 3306 -t 10 >> /tmp/DB.log") | crontab -
-    (crontab -l 2>/dev/null || echo ""; echo "* * * * *  ${azurerm_linux_virtual_machine.ticketing-jump01.public_ip_address}"  22 -t 10 >> /tmp/DB.log") | crontab -
   EOF
+
+  #  user_data = <<-EOF
+  #   #!/bin/bash
+  #   sudo yum update -y
+  #   sudo yum install -y httpd
+  #   sudo systemctl start httpd
+  #   sudo systemct enable httpd
+  #   sudo yum install telnet -y
+  #   sudo yum install cronie -y
+  #   sudo systemctl enable crond.service
+  #   sudo systemctl start crond.service
+  #   sudo yum install nc -y
+  #   (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.0.2.21 22 -t 10 >> /tmp/DB.log") | crontab -
+  #   (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.0.2.33 3306 -t 10 >> /tmp/DB.log") | crontab -
+  #   (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.10.4.43 3306 -t 10 >> /tmp/DB.log") | crontab -
+  #   (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.0.3.33 3306 -t 10 >> /tmp/DB.log") | crontab -
+  #   (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.0.3.23 3306 -t 10 >> /tmp/DB.log") | crontab -
+  #   (crontab -l 2>/dev/null || echo ""; echo "* * * * *  telnet 10.0.2.43 3306 -t 10 >> /tmp/DB.log") | crontab -
+  #   (crontab -l 2>/dev/null || echo ""; echo "* * * * *  ${azurerm_linux_virtual_machine.ticketing-jump01.public_ip_address}"  22 -t 10 >> /tmp/DB.log") | crontab -
+  # EOF
 }
