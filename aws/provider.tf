e7de0a21dd4354daf1ae1f84c62cbca8ab5c1f4f
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-07-10 10:33:58 -0400

    fixed

--- aws/provider.tf
+++ aws/provider.tf
@@ -23,10 +23,6 @@ provider "aws" {
 
 # # Generate random text for a unique storage account name
 resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
 
   byte_length = 8
 }
