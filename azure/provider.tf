542632fd80bbd18942cdadc4f235024f58989da0
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-07-10 09:21:35 -0400

    randomize all the names so there is no overlap.

--- azure/provider.tf
+++ azure/provider.tf
@@ -26,8 +26,19 @@ provider "azurerm" {
 provider "azuread" {
 }
 
+# # Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+
 locals  {
   # Extract the account ID prefix from the variable and create a bucket name
-  account_id_prefix = replace(split("@", var.account_id)[0],"+","")
-  storage_name = "instruqtflowlogs${local.account_id_prefix}"
+  account_id_prefix = replace(split("@", var.account_id)[0],"+","")+random_id.random_id.hex
+  storage_name = "instruqtsa${local.account_id_prefix}"
 }
