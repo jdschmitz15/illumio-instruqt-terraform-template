159ba9a8f7208e7693281a7b56db855835fdfde2
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-07-10 10:51:14 -0400

    update

--- aws/aws_flow_onboarding.tf
+++ aws/aws_flow_onboarding.tf
@@ -16,7 +16,7 @@ module "aws_flow_logs_s3_buckets" {
   role_id        = module.aws_account_onboarding.role_id
   iam_name_prefix       = local.account_id_prefix
   s3_bucket_arns = [
-    "arn:aws:s3:::${local.storage_name}",
+    "arn:aws:s3:::${local.storage_name}${random_id.random_id.hex}",
   ]
 
   depends_on = [aws_s3_bucket.s3bucket]
--- aws/provider.tf
+++ aws/provider.tf
@@ -30,5 +30,5 @@ resource "random_id" "random_id" {
 locals  {
   # Extract the account ID prefix from the variable and create a bucket name
   account_id_prefix = replace(split("@", var.account_id)[0],"+","")
-  storage_name = "instruqtsa-${local.account_id_prefix}-${random_id.random_id.hex}"
+  storage_name = "instruqtsa-${local.account_id_prefix}"
 }
