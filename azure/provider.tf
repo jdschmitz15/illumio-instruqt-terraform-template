3e6aa325ce6484ec70a11964553c2370a438e13b
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-11-13 15:52:12 -0500

    Update stroage account name so that the length is less than 22 characters and we add sa prefix

--- azure/provider.tf
+++ azure/provider.tf
@@ -41,8 +41,8 @@ resource "random_id" "random_id" {
 
 locals  {
   # Extract the account ID prefix from the variable and create a bucket name
-  account_id_prefix = replace(replace(split("@", var.account_id)[0], "+", ""), "-", "")
+  account_id_prefix = substr(replace(replace(split("@", var.account_id)[0], "+", ""), "-", ""), 0, 22)
   #account_id_prefix = replace(split("@", var.account_id)[0],"+","")
   #storage_name = "instruqtsa${local.account_id_prefix}${random_id.random_id.hex}"
-  storage_name = "instruqtsa${local.account_id_prefix}"
+  storage_name = "sa${local.account_id_prefix}"
 }
