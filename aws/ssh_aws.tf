9a9704ac06f720ea054e1c9b8f12c7de9eea3b89
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2026-01-19 13:06:40 -0500

    Fix the ssh key creation

--- aws/ssh_aws.tf
+++ aws/ssh_aws.tf
@@ -12,8 +12,7 @@ resource "random_pet" "ssh_key_name" {
 # Save private key locally (NOT in Terraform state)
 resource "local_file" "private_key" {
   content  = tls_private_key.ssh_key.private_key_pem
-  
-  filename = "$${HOME}/.ssh/${random_pet.ssh_key_name.id}-aws"
+  filename = "/root/.ssh/${random_pet.ssh_key_name.id}-aws"
   file_permission = "0600"
 }
 
